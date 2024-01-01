f5f6f8bc7a77ba796a358ec75e448d6b273332a3
Adrian Kurzac <adrian.kurzac@wp.pl>
2024-01-01 17:23:29 +0100

    Kurs SQL

--- SQL/Kurs_SQL_1.sql
+++ SQL/Kurs_SQL_1.sql
@@ -1,2 +1,5 @@
 
-SELECT  count(*) FROM movies where industry ="bollywood";
+SELECT  * FROM movies where title LIKE "%Captain%";
+select	distinct industry from movies;
+select * from movies where title like "%Thor%";
+select * from movies where industry = "Bollywood"
